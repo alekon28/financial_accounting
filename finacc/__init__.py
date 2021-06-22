63a3db189e8b27318fed8bdcff54342bcbf8b69b
diana <diananikkel958@gmail.com>
2021-06-22 15:18:14 +0700

    alerts and register form

--- finacc/__init__.py
+++ finacc/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/sasha/PycharmProjects/financial_accounting/database/main.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/diana/projects/financial_accounting/database/main.db'
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 
